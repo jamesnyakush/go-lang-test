f4b3a032db6b20302c31bdb0782dcb56ae31aec1
jamesnyakush <emvesttech@gmail.com>
2019-10-11 19:51:30 +0300

    learned maths operations in golang

---
+++ maths/operations.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println(1 + 1)  // 2
+	fmt.Println(2 * 4)  // 8
+	fmt.Println(8 - 4)  // 4
+	fmt.Println(2 % 6)  // 2
+	fmt.Println(12 / 4) // 3
+}
