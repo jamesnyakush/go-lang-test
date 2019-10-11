9dd66713e1097f9bcbdcb14487b6394f11060b7b
jamesnyakush <emvesttech@gmail.com>
2019-10-11 20:18:00 +0300

    learned variables

---
+++ variables/vairiables.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	// strings
+
+	var name string = "James Nyakundi"
+	fmt.Println(name)
+
+	//integers
+
+	var age int = 19
+	fmt.Println(age)
+
+	// boolean
+
+	var male bool = true
+	fmt.Println(male)
+
+}
