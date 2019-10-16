f21a38fa29f1cf0954d252625173f307dd537105
jamesnyakush <emvesttech@gmail.com>
2019-10-16 21:55:44 +0300

    working

---
+++ switch/switch.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	age := 18
+
+	switch age {
+	case 10:
+		fmt.Println("You are Under Age")
+	case 18:
+		fmt.Println("Yes Sell Juice to him")
+
+	}
+}
